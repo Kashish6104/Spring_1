635449af40dcbf08e4bdeab2efe87a5a1d7ade70
Kashish6104 <kashishs6104@gmail.com>
2024-09-22 10:29:35 +0530

    spring.xml

--- src/main/java/com/Dao/UserDao.java
+++ src/main/java/com/Dao/UserDao.java
@@ -1,9 +1,13 @@
-package com;
+package com.Dao;
 
 public class UserDao {
 
 	  static UserDao userDao = null;
 	 
+	  private UserDao() {
+		  
+	  }
+	  
 	 public  static UserDao userDao() {
 		 if (userDao == null){
 			 userDao = new UserDao(); 
